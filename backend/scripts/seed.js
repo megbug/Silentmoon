c5c7b72be7488a122a7deade39b3d26ce6eaf8a5
meghub <meghub@proton.me>
2023-06-29 15:48:33 +0200

    seed.js done and commented

--- backend/scripts/seed.js
+++ backend/scripts/seed.js
@@ -20,4 +20,35 @@ let gridFSBucket = new mongoose.mongo.GridFSBucket(mongoose.connection, {
 
 console.log('Connected to MongoDB');
 
+for await (let videoData of data.videos) {
 
+    // upload videofile to mongoDB using gridFS - filepath and filename are given by seed_test_data.json
+    // pipe offers reading from source and uploading to destination simultaneously
+    await fs.createReadStream(videoData.filepath).pipe(gridFSBucket.openUploadStream(videoData.filename));
+
+    // stat gets the statistical info about the file (e.g. filesize, creationtime, etc.)
+    const stat = await fs.statSync(videoData.filepath);
+
+    //generating a thumbnail filename
+    let thumbnail = videoData.filename.replace('mp4', 'png');
+    // generating a thumbnail image at the specified path
+    // handle the result of generating the thumbnail which is stored in a cachepath
+    // creates a read stream and pipes it to a gridFSBucket in MongoDB
+    await thumbsupply.generateThumbnail(videoData.filepath)
+        .then((cachePath) => {
+            fs.createReadStream(cachePath).pipe(gridFSBucket.openUploadStream(thumbnail));
+        });
+
+    let video = new Video({
+        title: videoData.title,
+        level: videoData.level,
+        category: videoData.category,
+        filename: videoData.filename,
+        filesize: stat.size,
+        thumbnail: thumbnail
+    });
+
+    await video.save();
+
+    mongoose.disconnect();
+}
