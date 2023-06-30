42f52e77489b7a5c45026061c09170bfda8ea4ff
meghub <meghub@proton.me>
2023-06-30 22:40:20 +0200

    made videofile available to play, when clicked

--- frontend/src/pages/Video.jsx
+++ frontend/src/pages/Video.jsx
@@ -1,8 +1,26 @@
+import axios from 'axios';
+import ReactPlayer from 'react-player/file';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
 const Video = () => {
-    
-    
-    return ( 
+    let { id } = useParams();
+    console.log(id)
+
+    const [video, setVideo] = useState({});
+    useEffect(() => {
+        axios.get(import.meta.env.VITE_BE_URL + `/api/yogavideos/${id}`)
+            .then((res) => setVideo(res.data))
+            .catch((err) => console.error(err))
+    }, [])
+
+    return (
         <>
+            <h3>Hier gibts videos</h3>
+            <ReactPlayer
+                url={import.meta.env.VITE_BE_URL + `/api/videostream/${video.filename}`}
+                controls={true}
+            />
         </>
     );
 }
