078e16a159dbcb852b6e1b6ab249b837b2d703c1
LanaxCode <lana.supercode@gmail.com>
2023-06-30 14:13:31 +0200

    fonts and icons

---
+++ frontend/src/components/HomeTopItem.jsx
@@ -0,0 +1,22 @@
+import '../sass/HomeTopItem.scss'
+import { Link } from 'react-router-dom';
+
+const HomeTopItem = () => {
+    return (
+        <div className="home_item" >
+            <h3 className='home_item_headline'>Healthy Back</h3>
+            <p>BEGINNER</p>
+            <div className='home_item_bottom'>
+                <p>3-10 MIN</p>
+                <Link to={`/login`}>
+                    <button>START</button>
+                </Link>
+            </div>
+
+        </div>
+    );
+}
+// style={{
+//     backgroundImage: `url(${image})`,
+// }} 
+export default HomeTopItem;
