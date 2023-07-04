daa7265b5778e6487ea978613ca6a73c70f5325b
meghub <meghub@proton.me>
2023-07-04 14:35:33 +0200

    deleted Logout Component

--- frontend/src/components/LogOut.jsx
+++ frontend/src/components/LogOut.jsx
@@ -1,22 +1,26 @@
 
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+// import axios from "axios";
+// import { useNavigate } from "react-router-dom";
+// import { useContext } from "react";
+// import { UserContext } from "./contexts/UserContext";
 
-const LogOut = () => {
-    const navigate = useNavigate()
+// const LogOut = () => {
+//     const { user } = useContext(UserContext);
+//     const navigate = useNavigate()
 
-    const logoutHandler = async () => {
-        try {
-            await axios.post(import.meta.env.VITE_BE_URL + "/api/logout");
-            navigate("/")
-        } catch (err) {
-            console.error(err)
-        }
-    }
+//     const logoutHandler = async () => {
+//         try {
+//             await axios.post(import.meta.env.VITE_BE_URL + "/api/logout", { withCredentials: true })
+//                 .then(() => setUser({}))
+//             navigate("/")
+//         } catch (err) {
+//             console.error(err)
+//         }
+//     }
 
-    return (
-        <button onClick={logoutHandler}>LogOut</button>
-    );
-}
+//     return (
+//         <button onClick={logoutHandler}>LogOut</button>
+//     );
+// }
 
-export default LogOut;
+// export default LogOut;
