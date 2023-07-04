bf249ab4ab2bed0a137c7315b0fea5003f449e6b
meghub <meghub@proton.me>
2023-07-04 14:39:56 +0200

    no need for api call - user comes from UserContext

--- frontend/src/pages/ProfilPage.jsx
+++ frontend/src/pages/ProfilPage.jsx
@@ -3,6 +3,7 @@ import { UserContext } from "../contexts/UserContext";
 import axios from "axios";
 
 import Navbar from '../components/Navbar';
+// import LogOut from "../components/LogOut";
 
 
 const ProfilPage = () => {
@@ -10,22 +11,23 @@ const ProfilPage = () => {
     console.log(user);
 
 
-    useEffect(() => {
-        axios.get("/api/verified", { withCredentials: true })
-            .then((res) => {
-                console.log(res);
-                setUser(res.data);
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    }, []);
+    // useEffect(() => {
+    //     axios.get("/api/verified", { withCredentials: true })
+    //         .then((res) => {
+    //             console.log(res);
+    //             // setUser(res.data);
+    //         })
+    //         .catch((err) => {
+    //             console.error(err);
+    //         });
+    // }, []);
 
 
     return (
         <>
             <h1>Profil von {user.name}</h1>
             <button onClick={logout}>Logout</button>
+            {/* <LogOut /> */}
             <Navbar />
         </>
     );
