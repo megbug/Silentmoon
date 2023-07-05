d0d3c61519be3a20849bff26a65d4552afae7c7b
meghub <meghub@proton.me>
2023-07-05 10:47:53 +0200

    added Video Slider

---
+++ frontend/src/components/VideoSlider.jsx
@@ -0,0 +1,34 @@
+import '../sass/Slider.scss'
+import VideoSliderItem from './VideoSliderItem.jsx';
+import axios from 'axios'
+import { useEffect, useState } from 'react';
+
+const VideoSlider = () => {
+    const [videos, setVideos] = useState([]);
+    useEffect(() => {
+        axios.get(import.meta.env.VITE_BE_URL + `/api/yogavideos`, { withCredentials: true })
+            .then((res) => setVideos(res.data))
+            .catch((err) => console.error(err))
+        // if level or category changes through button click the api call retrieves the new asked for data
+    }, [])
+
+    return (
+        <>
+            <article className='slider'>
+                {videos.length > 0 && videos.map((item, i) => {
+                    return (
+                        <VideoSliderItem
+                            key={i}
+                            id={item._id}
+                            category={item.category}
+                            thumbnail={item.thumbnail}
+                            level={item.level}
+                        />
+                    )
+                })}
+            </article>
+        </>
+    );
+}
+
+export default VideoSlider;
