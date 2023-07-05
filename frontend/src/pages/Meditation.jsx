2c0ea7915a651fc4d4a4596d8229ef3071d48f7d
LanaxCode <lana.supercode@gmail.com>
2023-07-05 14:11:54 +0200

    meditation and yogasite

--- frontend/src/pages/Meditation.jsx
+++ frontend/src/pages/Meditation.jsx
@@ -1,11 +1,18 @@
 import Navbar from '../components/Navbar'
+import Gallery from '../components/Gallery';
 
 const Meditation = () => {
 
 
     return (
         <>
-            <h1>Meditation</h1>
+            <section className='yoga_site_container'>
+                <h1 className='logoDark'>SILENT MOON</h1>
+                <h2 className='hdl-gray-sites'>Meditation</h2>
+                <h3 className='subhead-sites'>Audio-only meditation techniques to help you minimize your screen time and practice on the go.</h3>
+
+                <Gallery />
+            </section>
             <Navbar />
         </>
     );
