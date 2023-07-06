ea2aa9c89eb6c364f5725d6eef26a3bce5101b53
meghub <meghub@proton.me>
2023-07-06 22:44:35 +0200

    added class for styling purpose

--- frontend/src/components/FavVideoSliderItem.jsx
+++ frontend/src/components/FavVideoSliderItem.jsx
@@ -5,7 +5,7 @@ const FavVideoSliderItem = (props) => {
     return (
         <div className="slider_item_container">
             <Link to={`/video/${props.id}`}><img src={import.meta.env.VITE_BE_URL + `/api/thumbnail/${props.thumbnail}`} alt="thumbnail" className="slider_img" /></Link>
-            <h3>{props.category}</h3>
+            <h3 className='category'>{props.category}</h3>
             <div className='slider_description'>
                 <p>{props.level}</p>
             </div>
