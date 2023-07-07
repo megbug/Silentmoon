d104bf95912ce24b3ca9d58caae8d11159d7d024
Maartala <Steffi.Gerstenmaier@t-online.de>
2023-07-07 18:59:39 +0200

    DailyCalm randomVideo hinterlegt

--- frontend/src/components/DailyCalm.jsx
+++ frontend/src/components/DailyCalm.jsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import '../sass/DailyCalm.scss'
 import { Link } from 'react-router-dom';
 import playButton from '../assets/img/daily_calm_button.svg'
 
-const DailyCalm = (props) => {
+const DailyCalm = () => {
+    const [randomVideo, setRandomVideo] = useState(null);
+
+    useEffect(() => {
+        axios
+            .get(import.meta.env.VITE_BE_URL + `/api/yogavideos`, { withCredentials: true })
+            .then((res) => {
+                if (res.data.length > 0) {
+                    const randomIndex = Math.floor(Math.random() * res.data.length);
+                    setRandomVideo(res.data[randomIndex]);
+                }
+            })
+            .catch((err) => console.error(err));
+    }, []);
+    if (!randomVideo) {
+        return null;
+    }
+
     const currentDate = new Date();
     const month = currentDate.toLocaleString('default', { month: 'long' });
     const day = currentDate.getDate();
@@ -13,7 +33,7 @@ const DailyCalm = (props) => {
                 <p className='day_month_daily'>{`${month} ${day}`}</p>
             </div>
             <div>
-                <Link to={`/video/${props.id}`}><img src={playButton} className='play_button_dailycalm'></img></Link>
+                <Link to={`/video/${randomVideo._id}`}><img src={playButton} className='play_button_dailycalm'></img></Link>
             </div>
         </section>
     );
