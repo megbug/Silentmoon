63c27b51625901da6ca4a41fbd362e5cc85c66ae
LanaxCode <lana.supercode@gmail.com>
2023-07-08 13:13:33 +0200

    finish

--- frontend/src/components/MeditationSliderItem.jsx
+++ frontend/src/components/MeditationSliderItem.jsx
@@ -5,7 +5,7 @@ const MeditationSliderItem = (props) => {
     return (
         <div className="slider_item_container">
             <Link to={`/music`}><img src={import.meta.env.VITE_BE_URL + `/api/image/${props.filename}`} alt="thumbnail" className="slider_img" /></Link>
-            <h2 className='category'>{props.category}</h2>
+            <h2 className='category'>{props.title}</h2>
             <div>
                 <p className='slider_description'>{props.level}</p>
             </div>
