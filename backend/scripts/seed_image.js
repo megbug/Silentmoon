fe3871cc0fb245a4fe12a589384fa8faeb2a67c2
megbug <meghub@proton.me>
2023-07-27 18:02:51 +0200

    deleted and corrected comments - changed filenames for better readability

--- backend/scripts/seed_image.js
+++ backend/scripts/seed_image.js
@@ -1,16 +1,12 @@
-import "../config/config.js"
-import fs from 'fs'
+import '../config/config.js';
+
+import fs from 'fs';
 import mongoose from 'mongoose';
-import thumbsupply from 'thumbsupply'
 
-import data from './data_img.json' assert {type: 'json'};
-import { Image } from '../model/Image.js'
-import { log } from "console";
+import data from './seed_data_img.json' assert {type: 'json'};
+import { Image } from '../model/Image.js';
 
-mongoose.connect(process.env.DB)
-
-
-
+mongoose.connect(process.env.DB);
 
 // GridFSBucket provides methods for working with files stored in the bucket
 // creating this bucket allows to perfom tasks as retrieving, updating etc. in the MongoDB using GridFS storage system
@@ -23,22 +19,13 @@ console.log('Connected to MongoDB');
 
 for await (let imageData of data.images) {
 
-    // upload videofile to mongoDB using gridFS - filepath and filename are given by seed_test_data.json
+    // upload imagefile to mongoDB using gridFS - filepath and filename are given by seed_data_img.json
     // pipe offers reading from source and uploading to destination simultaneously
     await fs.createReadStream(imageData.filepath).pipe(gridFSBucket.openUploadStream(imageData.filename));
 
     // stat gets the statistical info about the file (e.g. filesize, creationtime, etc.)
     const stat = await fs.statSync(imageData.filepath);
 
-    // generating a thumbnail filename
-    // let thumbnail = imageData.filename;
-    // // generating a thumbnail image at the specified path
-    // // handle the result of generating the thumbnail which is stored in a cachepath
-    // // creates a read stream and pipes it to a gridFSBucket in MongoDB
-    // await thumbsupply.generateThumbnail(imageData.filepath)
-    //     .then((cachePath) => {
-    //         fs.createReadStream(cachePath).pipe(gridFSBucket.openUploadStream(thumbnail));
-    //     });
 
     let image = new Image({
         title: imageData.title,
@@ -46,11 +33,10 @@ for await (let imageData of data.images) {
         category: imageData.category,
         filename: imageData.filename,
         filesize: stat.size,
-        // thumbnail: thumbnail,
         description: imageData.description
     });
 
     await image.save();
 
-    // mongoose.disconnect();
+    mongoose.disconnect();
 }
--- backend/scripts/seed_video.js
+++ backend/scripts/seed_video.js
@@ -1,14 +1,13 @@
-import "../config/config.js"
-import fs from 'fs'
+import '../config/config.js';
+
+import fs from 'fs';
 import mongoose from 'mongoose';
 import thumbsupply from 'thumbsupply'
 
-import data from './seed_data.json' assert {type: 'json'};
-import { Video } from '../model/Video.js'
+import data from './seed_data_video.json' assert {type: 'json'};
+import { Video } from '../model/Video.js';
 
-
-mongoose.connect(process.env.DB)
-
+mongoose.connect(process.env.DB);
 
 // GridFSBucket provides methods for working with files stored in the bucket
 // creating this bucket allows to perfom tasks as retrieving, updating etc. in the MongoDB using GridFS storage system
@@ -21,7 +20,7 @@ console.log('Connected to MongoDB');
 
 for await (let videoData of data.videos) {
 
-    // upload videofile to mongoDB using gridFS - filepath and filename are given by seed_test_data.json
+    // upload videofile to mongoDB using gridFS - filepath and filename are given by seed_data_video.json
     // pipe offers reading from source and uploading to destination simultaneously
     await fs.createReadStream(videoData.filepath).pipe(gridFSBucket.openUploadStream(videoData.filename));
 
@@ -30,6 +29,7 @@ for await (let videoData of data.videos) {
 
     //generating a thumbnail filename
     let thumbnail = videoData.filename.replace('mp4', 'png');
+    
     // generating a thumbnail image at the specified path
     // handle the result of generating the thumbnail which is stored in a cachepath
     // creates a read stream and pipes it to a gridFSBucket in MongoDB
@@ -50,5 +50,5 @@ for await (let videoData of data.videos) {
 
     await video.save();
 
-    // mongoose.disconnect();
+    mongoose.disconnect();
 }
