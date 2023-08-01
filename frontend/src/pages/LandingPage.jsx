b7bd466c98db40e8dc64669c35eecd75057ec7f4
megbug <meghub@proton.me>
2023-08-01 11:49:02 +0200

    deleted comments and unnecessary code

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,42 +1,21 @@
-import '../sass/LandingPage.scss'
-import '../sass/mediaquerie.scss'
+import { useState } from 'react'
+import { Link } from 'react-router-dom';
+
 import SignUp from '../components/SignUp'
 import LogIn from '../components/LogIn'
-import { useState } from 'react'
 
-import { Link } from "react-router-dom";
+import '../sass/LandingPage.scss'
+import '../sass/mediaquerie.scss'
 
 const LandingPage = () => {
 
-    console.log(window)
-
-    let isDesktop;
-
-    if (window.screen.width > 390) {
-        isDesktop = true;
-        console.log('big screen')
-    }
+    let isDesktop = window.screen.width > 390
 
     const [accessRoute, setAccessRoute] = useState('');
 
     return (
-        // <section className='landingContainer'>
-        //     <div className='handstand'>
-        //     </div>
-        //     <div className='accessArea'>
-        //         <h1 className='logoDark'>SILENT MOON</h1>
-        //         <h2 className='intro-Hdl'>We are what we do</h2>
-        //         <p className='intro-Text'>Thousands of people are using silent moon <br /> for meditation and yoga classes.</p>
-        //         <button onClick={()=> setAccessRoute('SignUp')}>SignUp</button>
-        //         <button onClick={()=> setAccessRoute('LogIn')}>LogIn</button>
-        //         {/* <Link to={"/signup"} className="intro-Btn-Link" ><button className='bigBtn-red'>SIGN UP</button></Link> */}
-        //         {/* <Link to={"/login"} className="intro-Link">ALREADY HAVE AN ACCOUNT? &nbsp; <span> LOG IN</span></Link> */}
-        //     </div>
-        // </section>
-
         <section className='landingContainer'>
-            <div className='handstand'>
-            </div>
+            <div className='handstand'></div>
             <div className='accessArea'>
                 <h1 className='logoDark'>SILENT MOON</h1>
                 <h2 className='intro-Hdl'>We are what we do</h2>
@@ -45,23 +24,20 @@ const LandingPage = () => {
                 {isDesktop ? 
                     <div className='accessDiv'>
                         <button onClick={()=> setAccessRoute('SignUp')}>Sign Up</button>
-                        <div className='accessSignUp'>{accessRoute == 'SignUp' && (<SignUp property={isDesktop}></SignUp>)}</div>
+                        <div className='accessSignUp'>{accessRoute == 'SignUp' && (<SignUp/>)}</div>
                     
                         <button onClick={()=> setAccessRoute('LogIn')}>Log In</button>
-                        <div className='accessLogIn'>{accessRoute == 'LogIn' && (<LogIn></LogIn>)}</div>
+                        <div className='accessLogIn'>{accessRoute == 'LogIn' && (<LogIn/>)}</div>
                     </div>
                 : 
                     <div>
-                        <Link to={"/signup"} className="intro-Btn-Link" ><button className='bigBtn-red'>SIGN UP</button></Link>
-                        <Link to={"/login"} className="intro-Link">ALREADY HAVE AN ACCOUNT? &nbsp; <span> LOG IN</span></Link>
+                        <Link to={'/signup'} className='intro-Btn-Link' ><button className='bigBtn-red'>SIGN UP</button></Link>
+                        <Link to={'/login'} className='intro-Link'>ALREADY HAVE AN ACCOUNT? &nbsp; <span> LOG IN</span></Link>
                     </div>
                 }
-                
-
             </div>
         </section>
     )
-    
 }
 
 export default LandingPage;
