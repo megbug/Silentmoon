c5a691149438ed746fd0d2d9029b7f42c47a610f
megbug <meghub@proton.me>
2023-08-10 17:03:59 +0200

    changed yoga/medi pages to use gallery and searchbar component

--- frontend/src/pages/Meditation.jsx
+++ frontend/src/pages/Meditation.jsx
@@ -1,21 +1,52 @@
-import Navbar from '../components/Navbar'
-import GalleryMeditation from '../components/GalleryMeditation.jsx';
+import { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+
+import { UserContext } from "../contexts/UserContext.jsx";
+
 import Logo from '../components/Logo';
+import FilterButton from '../components/FilterButton';
+import Searchbar from '../components/SearchBar';
+import DailyCalm from '../components/DailyCalm';
+import Gallery from '../components/Gallery';
+import Navbar from '../components/Navbar'
+
+import "../sass/Meditation.scss";
 
 const Meditation = () => {
+    const { user } = useContext(UserContext);
+
+    const [meditations, setMeditations] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [filters, setFilters] = useState({});
+
+    useEffect(()=> {
+        axios.get(import.meta.env.VITE_BE_URL + `/api/meditationimages?level=${filters.level}&category=${filters.category}&favMeditations=${filters.favs}`, {withCredentials: true})
+        .then((res) => {setMeditations(res.data)})
+        .catch((err) => console.error(err))
+   }, [filters]);
+
+   const filterMeditations = meditations.filter((item) => {
+    let level = item.level.toLowerCase();
+    let category = item.category.toLowerCase();
+    let description = item.description.toLowerCase();
+    let search = searchTerm.toLowerCase();
+    return level.includes(search) || category.includes(search) || description.includes(search);
+   });
 
 
     return (
-        <>
-            <section className='yoga_site_container'>
-                <Logo/>
-                <h2 className='hdl-gray-sites'>Meditation</h2>
+        <section className='pageWrapper'>
+            <Logo/>
+            <section className='meditationPage'>
+                <h2 className='pageHeadline'>Meditation</h2>
                 <h3 className='subhead-sites'>Audio-only meditation techniques to help you minimize your screen time and practice on the go.</h3>
-
-                <GalleryMeditation />
+                <FilterButton filters={filters} setFilters={setFilters}/>
+                <Searchbar searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+                <DailyCalm />
+                <Gallery items={filterMeditations} isVideo={false}/>
             </section>
             <Navbar />
-        </>
+        </section>
     );
 }
 
--- frontend/src/pages/Yoga.jsx
+++ frontend/src/pages/Yoga.jsx
@@ -1,27 +1,48 @@
-import Gallery from '../components/Gallery';
-import Logo from '../components/Logo';
-import Navbar from '../components/Navbar'
-// import "../sass/Yoga.scss";
-import Searchbar from '../components/SearchBar';
+import { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
 
+import { UserContext } from "../contexts/UserContext.jsx";
+
+import Logo from '../components/Logo';
+import FilterButton from '../components/FilterButton';
+import Searchbar from '../components/SearchBar';
+import DailyCalm from '../components/DailyCalm';
+import Gallery from '../components/Gallery';
+import Navbar from '../components/Navbar'
+
+import "../sass/Yoga.scss";
 
 const Yoga = () => {
+    const { user } = useContext(UserContext);
 
-    // return (
-    //     <section className="pageWrapper">
-    //         Ich bin die yoga seite
-    //     </section>
-    // )
+    const [videos, setVideos] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [filters, setFilters] = useState({});
 
+    useEffect(()=> {
+        axios.get(import.meta.env.VITE_BE_URL + `/api/yogavideos?level=${filters.level}&category=${filters.category}&favVideos=${filters.favs}`, {withCredentials: true})
+        .then((res) => {setVideos(res.data)})
+        .catch((err) => console.error(err))
+   }, [filters]);
+
+   const filterVideos = videos.filter((item) => {
+    let level = item.level.toLowerCase();
+    let category = item.category.toLowerCase();
+    let description = item.description.toLowerCase();
+    let search = searchTerm.toLowerCase();
+    return level.includes(search) || category.includes(search) || description.includes(search);
+   });
 
     return (
         <section className='pageWrapper'>
-            <section className='yoga_site_container'>
-                <Logo/>
+            <Logo/>
+            <section className='yogaPage'>
                 <h2 className='pageHeadline'>Yoga</h2>
                 <h3 className='text'>Find your inner zen from annywhere.</h3>
-
-                <Gallery />
+                <FilterButton filters={filters} setFilters={setFilters}/>
+                <Searchbar searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+                <DailyCalm />
+                <Gallery items={filterVideos} isVideo={true}/>
             </section>
             <Navbar />
         </section>
