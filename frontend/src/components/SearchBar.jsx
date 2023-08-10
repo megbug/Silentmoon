fc816fe142d078c0c206dd9c0d4eaaeb8ebb5657
megbug <meghub@proton.me>
2023-08-10 13:44:52 +0200

    changed Searchbar for better reusability

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,24 +1,14 @@
-import { useState } from "react";
 // import '../sass/SearchBar.scss'
 
-const Searchbar = ({ onSearch }) => {
-    const [searchTerm, setSearchTerm] = useState("");
+const Searchbar = (props) => {
+    const {searchTerm, setSearchTerm} = props;
 
     const handleSearch = (e) => {
-        const value = e.target.value;
-        setSearchTerm(value);
-        onSearch(value);
+        setSearchTerm(e.target.value);
     };
 
     return (
-        <div className="search-container">
-            <input
-                type="text"
-                value={searchTerm}
-                onChange={handleSearch}
-                className="searchInput"
-            />
-        </div>
+        <input type="text" value={searchTerm} onChange={handleSearch} placeholder="looking for something?"/>
     );
 };
 
